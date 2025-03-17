4c46d0a8157c7f0670e3d4c5dc0f624fa3117a1c
root <root@ip-172-31-9-108.ap-south-1.compute.internal>
2025-03-17 15:44:14 +0000

    Add variables to main file

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami           = "ami-0b6d6dacf350ebc82"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 }
 
---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "ami_id" {
+  type    = string
+  default = "ami-0b6d6dacf350ebc82"
+}
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
